d9935d4ffd2d3383fce67c41988ce8fe0cbe058c
Gabor Szabo <gabor@szabgab.com>
2024-05-02 10:51:53 +0300

    separate the display function

--- day03/rectangle_in_functions.py
+++ day03/rectangle_in_functions.py
@@ -1,10 +1,13 @@
 
+def display(area):
+    print(f"The area is {area}")
+
 def main():
     length = input("Length: ")
     width = input("Width: ")
 
     area = int(length) * int(width)
-    print(f"The area is {area}")
+    display(area)
 
 
 main()
