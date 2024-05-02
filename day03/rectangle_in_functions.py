f029bec6e4cda560f1eb285486524887e7be1a0d
Gabor Szabo <gabor@szabgab.com>
2024-05-02 10:58:39 +0300

    rename variable to show that they are unrelated

--- day03/rectangle_in_functions.py
+++ day03/rectangle_in_functions.py
@@ -2,9 +2,14 @@
 def display(data):
     print(f"The area is {data}")
 
+def get_input():
+    received_length = input("Length: ")
+    received_width = input("Width: ")
+
+    return (received_length, received_width)
+
 def main():
-    length = input("Length: ")
-    width = input("Width: ")
+    (length, width) = get_input()
 
     area = int(length) * int(width)
     display(area)
