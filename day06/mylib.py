09ea2eb764db5d93d676e0fed0af04922007232c
Gabor Szabo <gabor@szabgab.com>
2024-05-23 09:22:28 +0200

    handle exception

--- day06/mylib.py
+++ day06/mylib.py
@@ -1,15 +1,24 @@
 def process_file(filename):
     print(filename)
-    try:
-        with open(filename) as fh:
-            content = fh.read()
-    except FileNotFoundError:
-        print(f"File {filename} was not found")
-        return
-
-    if int(content) == 0:
-        print("Can't divide by zero")
-        return
-    
+    with open(filename) as fh:
+        content = fh.read()
+   
     result = 100 / int(content)
     print(f"100 / {content}  = {result}")
+
+
+# def process_file(filename):
+#     print(filename)
+#     try:
+#         with open(filename) as fh:
+#             content = fh.read()
+#     except FileNotFoundError:
+#         print(f"File {filename} was not found")
+#         return
+
+#     if int(content) == 0:
+#         print("Can't divide by zero")
+#         return
+    
+#     result = 100 / int(content)
+#     print(f"100 / {content}  = {result}")
--- day06/process_text_files.py
+++ day06/process_text_files.py
@@ -4,7 +4,29 @@ from mylib import process_file
 def main():
     #print(sys.argv)
     for filename in sys.argv[1:]:
-        process_file(filename)
+        try:
+            process_file(filename)
+        # except FileNotFoundError:
+        #     print(f"File {filename} was not found")
+        # except ZeroDivisionError:
+        #     print("Can't divide by zero")
+        #     #break
+        except Exception as err:
+            print(f"There was some other exception: {err}")
+
+
+
+# def main():
+#     #print(sys.argv)
+#     for filename in sys.argv[1:]:
+#         try:
+#             process_file(filename)
+#         except FileNotFoundError:
+#             print(f"File {filename} was not found")
+#         except ZeroDivisionError:
+#             print("Can't divide by zero")
+#             #break
+
 
 
 main()
