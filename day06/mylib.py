d0d8a7e666d003bb8a4be892d1307d04cc49b76f
Gabor Szabo <gabor@szabgab.com>
2024-05-23 08:47:18 +0200

    separate library

---
+++ day06/mylib.py
@@ -0,0 +1,15 @@
+def process_file(filename):
+    print(filename)
+    try:
+        with open(filename) as fh:
+            content = fh.read()
+    except FileNotFoundError:
+        print(f"File {filename} was not found")
+        return
+
+    if int(content) == 0:
+        print("Can't divide by zero")
+        return
+    
+    result = 100 / int(content)
+    print(f"100 / {content}  = {result}")
--- day06/process_text_files.py
+++ day06/process_text_files.py
@@ -1,24 +1,10 @@
 import sys
+from mylib import process_file
 
 def main():
     #print(sys.argv)
     for filename in sys.argv[1:]:
         process_file(filename)
 
-def process_file(filename):
-    print(filename)
-    try:
-        with open(filename) as fh:
-            content = fh.read()
-    except FileNotFoundError:
-        print(f"File {filename} was not found")
-        return
-
-    if int(content) == 0:
-        print("Can't divide by zero")
-        return
-    
-    result = 100 / int(content)
-    print(f"100 / {content}  = {result}")
 
 main()
